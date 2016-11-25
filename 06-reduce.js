0256d14aa20f20405c7947678913d3ea01317f37
Maynagashev <lifesup@gmail.com>
2016-11-25 16:59:02 +0700

    reduce

---
+++ 06-reduce.js
@@ -0,0 +1,40 @@
+/**
+ * Created by https://github.com/maynagashev on 25.11.2016.
+ */
+
+/*
+# Task
+
+Given an Array of strings, use Array#reduce to create an object that contains the number of times each string occured in the array. Return the object directly (no need to console.log).
+
+## Example
+
+var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
+
+console.log(countWords(inputWords))
+
+// =>
+// {
+//   Apple: 2,
+//   Banana: 1,
+//   Durian: 3
+// }
+
+## Arguments
+
+* inputWords: An array of random Strings.
+
+## Conditions
+
+* Do not use any for/while loops or Array#forEach.
+* Do not create any unnecessary functions e.g. helpers.
+* */
+function countWords(inputWords) {
+   return inputWords.reduce(function (t, c) {
+       t[c] = (t[c]) ? t[c]+1 : 1;
+       // t[c] = ++t[c] || 1;  // from example
+       return t;
+   }, {});
+}
+
+module.exports = countWords
